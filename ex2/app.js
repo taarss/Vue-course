e6a3646f3e7682db491ae46968d91f8a9f8e7235
Christian Villadsen <christianbjerrumvilladsen@hotmail.com>
2021-05-11 11:03:45 +0200

    Finished ex2

--- ex2/app.js
+++ ex2/app.js
@@ -1,8 +1,15 @@
-const addBtn = document.querySelector("button").addEventListener(`click`, 
-function () {
-   let listElement = document.createElement("li");
-   listElement.innerHTML = document.querySelector("#goal").value;
-   document.querySelector("#app ul").appendChild(listElement);
-   
-}
-);
+Vue.createApp({
+   data() {
+      return {
+         goals: [],
+         enteredValue: ''
+      };
+   },
+   methods: {
+      addGoal() {
+         this.goals.push(this.enteredValue);
+      }
+   }
+}).mount('#app');
+
+
