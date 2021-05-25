cbd2cc102f6a0731424b9891e343bd46ed5cf8d3
Christian Bjerrum Villadsen <chri45n5@edu.campusvejle.dk>
2021-05-25 17:06:40 +0200

    working with fragments

--- 9cmp/src/App.vue
+++ 9cmp/src/App.vue
@@ -1,40 +1,32 @@
 <template>
   <div>
     <TheHeader/>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals> 
-      <template #default="slotProps">
-        <h2>
-          {{ slotProps.item }}
-        </h2>
-        <p>{{ slotProps['another-prop'] }}</p>
-      </template>
-    </course-goals>
-
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent">
+        
+      </component>
+    </keep-alive>
+    
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader';
-import BadgeList from './components/BadgeList';
-import UserInfo from './components/UserInfo';
-import CourseGoals from './components/CourseGoals';
+import ActiveGoals from './components/ActiveGoals';
+import ManageGoals from './components/ManageGoals';
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals
-    ,
+    ActiveGoals,
+    ManageGoals,
+    
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -42,6 +34,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp){
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
